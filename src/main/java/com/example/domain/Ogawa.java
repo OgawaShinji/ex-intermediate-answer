1db76eba072adc42db9a71386fe4a28238b3361f
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-07-09 14:02:46 +0900

    create domain/Ogawa.java

---
+++ src/main/java/com/example/domain/Ogawa.java
@@ -0,0 +1,12 @@
+package com.example.domain;
+public class Ogawa {
+    private String wakio;
+
+    public String getWakio() {
+        return wakio;
+    }
+
+    public void setWakio(String wakio) {
+        this.wakio = wakio;
+    }
+}

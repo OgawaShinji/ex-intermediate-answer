853f793f89b626587661aa66181de03d4cf58919
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-07-09 13:49:06 +0900

    Arisaレポジトリー追加

---
+++ src/main/java/com/example/domain/Arisa.java
@@ -0,0 +1,14 @@
+package com.example.domain;
+
+public class Arisa {
+
+	private String arisa;
+
+	public String getArisa() {
+		return arisa;
+	}
+
+	public void setArisa(String arisa) {
+		this.arisa = arisa;
+	}
+}

39e028af3840f0d93d213b8fdc41f9b580f7dafa
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-07-09 17:30:55 +0900

    create findBylessThanPrice test

--- src/test/java/com/example/ExIntermediateApplicationTests.java
+++ src/test/java/com/example/ExIntermediateApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.domain.*;
+import com.example.repository.HotelRepository;
+
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExIntermediateApplicationTests {
-
+	@Before
+	public void setUp() throws Exception {}
+	@After
+	public void tearDown() throws Exception {}
 	@Test
 	public void contextLoads() {
+		
+	}
+	@Autowired
+	private HotelRepository hotelRepository;
+	@Test
+	public void findByLessThanPrice_正常系_引数5000(){
+		Hotel hotel=hotelRepository.findByLessThanPrice(5000).get(0);
+		assertEquals("error","ホテルローズガーデン新宿", hotel.getHotelName());
 	}
 
 }
